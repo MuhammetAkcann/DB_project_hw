d04bceed806d1c53396f6b2fbdd2bdbf74336179
MuhammetAkcann <akcan18@itu.edu.tr>
2021-01-19 16:15:07 +0300

    python 3 convertion

--- server.py
+++ server.py
@@ -17,7 +17,7 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 
 
-HEROKU = True
+HEROKU = False
 if(not HEROKU):
     os.environ['DATABASE_URL'] = "dbname='postgres' user='postgres' host='localhost' password='1234'"
     initialize(os.environ.get("DATABASE_URL"))
@@ -33,15 +33,15 @@ def appointment_page(id=0,type=0):
     patientt = queries.select("name","patient",asDict = True)
     total_user = len(psychologistt) + len(patientt)
     print("W"*30)
-    print(request.form.items())
+    print(list(request.form.items()))
     psychologist = queries.select("id,name,mail","psychologist",asDict=True)
     if(request.method == "GET"):
         return render_template("appointment.html",active_num = active_num,total_user=total_user,psychologist = psychologist,id=id)
 
     psychologist = queries.select("id,name","psychologist",asDict=True)
     print("?-"*26)
-    print("request:",dict(request.form.items()))
-    requests = dict(request.form.items())
+    print("request:",dict(list(request.form.items())))
+    requests = dict(list(request.form.items()))
     psy = queries.select("id","psychologist","mail = '{}'".format(requests["mail"]),asDict=True)
     print(psy)
     queries.insert("appointment","psychologist_id,patient_id,day,time","{},{},{},{}".format(psy["id"],session["id"],requests["day"],requests["time"]))
@@ -58,8 +58,8 @@ def patient_page(id):
     patientt = queries.select("name","patient",asDict = True)
     total_user = len(psychologistt) + len(patientt)
     if(request.method == "POST"):
-        requests = dict(request.form.items())
-        delete_one = requests.keys()[0]
+        requests = dict(list(request.form.items()))
+        delete_one = list(requests.keys())[0]
         queries.delete("appointment","appointment.id={}".format(delete_one))
     patients = queries.select("id,name,mail","patient","id={}".format(id),asDict=True)
     appointments = queries.select("id,day,time,patient_id,psychologist_id","appointment","appointment.patient_id={}".format(id),asDict = True)
@@ -104,10 +104,10 @@ def psychologist_page(id):
     total_user = len(psychologistt) + len(patientt)
     if(request.method == "POST"):
         if("comment" in request.form):
-            requests = dict(request.form.items())
+            requests = dict(list(request.form.items()))
             queries.insert("comment","patient_id,psychologist_id,comment","'{}','{}','{}'".format(session["id"],id,requests["comment_content"]))
         elif("point" in request.form):
-            requests = dict(request.form.items())
+            requests = dict(list(request.form.items()))
             selected = queries.select("id","point","patient_id = {} AND psychologist_id = {}".format(session["id"],id))
             print("SEEEELELLLLLLLEEEECCCCCTTEEEEEDDDDDDDD")
             print(selected)
@@ -118,17 +118,17 @@ def psychologist_page(id):
                 queries.update("point","point = {}".format(requests["point"]),"patient_id = {} AND psychologist_id = {}".format(session["id"],id))
         elif("give_Add" in request.form):
             adds.append(id)
-        elif(request.form.items()[0][1]=="delete"):
-            com_id = request.form.items()[0][0]
+        elif(list(request.form.items())[0][1]=="delete"):
+            com_id = list(request.form.items())[0][0]
             queries.delete("comment","comment.id = {}".format(com_id))
-        elif("Cancel" in request.form.items()[0]):
-            requests = dict(request.form.items())
-            delete_one = requests.keys()[0]
+        elif("Cancel" in list(request.form.items())[0]):
+            requests = dict(list(request.form.items()))
+            delete_one = list(requests.keys())[0]
             queries.delete("appointment","appointment.id={}".format(delete_one))    
         else:
-            print("HA"*30,request.form.items())
-            queries.update("comment","comment = '{}'".format(request.form.items()[0][1]),"id = {}".format(request.form.items()[1][0]) )
-            queries.update("comment","comment = '{}'".format(request.form.items()[1][1]),"id = {}".format(request.form.items()[0][0]) )
+            print("HA"*30,list(request.form.items()))
+            queries.update("comment","comment = '{}'".format(list(request.form.items())[0][1]),"id = {}".format(list(request.form.items())[1][0]) )
+            queries.update("comment","comment = '{}'".format(list(request.form.items())[1][1]),"id = {}".format(list(request.form.items())[0][0]) )
         
     psychologist = queries.select("id,name,mail,address","psychologist","id={}".format(id),asDict=True)
     point = queries.select("point","point","psychologist_id = {}".format(id),asDict = False)
@@ -289,7 +289,7 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         string = 'Account created for ' + form.name.data
-        requests = dict(request.form.items())
+        requests = dict(list(request.form.items()))
         print("AB"*30)
         print(requests)
         if requests["user_type"] == "psychologist":
